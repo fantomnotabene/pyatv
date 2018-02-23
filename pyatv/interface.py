874ed1c12fa922a9c5d5f2dd3698244869900305
Pierre Ståhl <pierre.staahl@gmail.com>
2018-02-23 14:55:08 +0100

    Suspend support for MRP

--- pyatv/interface.py
+++ pyatv/interface.py
@@ -144,6 +144,11 @@ class RemoteControl(object):
         raise exceptions.NotSupportedError
 
     @abstractmethod
+    def suspend(self):
+        """Suspend the device."""
+        raise exceptions.NotSupportedError
+
+    @abstractmethod
     def set_position(self, pos):
         """Seek in the current playing media."""
         raise exceptions.NotSupportedError
